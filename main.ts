8846314957ee0aec86220b2289d915c6b943b648
FerdinandBujanowski <ferdinand-bujanowski@web.de>
2024-04-22 11:02:58 +0200

    included old code

--- main.ts
+++ main.ts
@@ -1,89 +1,186 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	MarkdownView,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DefinitionSettings {
+	filename: string;
+	def_color: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: DefinitionSettings = {
+	filename: "Definitions",
+	def_color: "#0000FF",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const def_dict: any = {
+	test: "This is a test definition.",
+};
+
+export default class DefinitionPlugin extends Plugin {
+	settings: DefinitionSettings;
+
+	async scanDefinitions() {
+		let counter = 0;
+
+		const files = this.app.vault.getMarkdownFiles();
+		for (let file of files) {
+			if (file.basename === this.settings.filename) {
+				await this.app.vault.read(file).then((content: string) => {
+					const lines = content.split("\n");
+					for (let line of lines) {
+						if (line.contains(":")) {
+							const [left, right] = line.split(":");
+
+							const words_left = left
+								.replace(/[*_#´]/g, "")
+								.split(",");
+							for (let word of words_left) {
+								const word_trim = word.trim();
+								def_dict[word_trim.toLowerCase()] = right;
+							}
+							counter++;
+						}
+					}
+
+					// write json to new file for debug purposes
+					// const newPath = file.path.replace(".md", "_json.md");
+					// this.app.vault.create(newPath, JSON.stringify(def_dict));
+				});
+			}
+		}
+		new Notice("Found " + counter.toString() + " definitions.");
+	}
+
+	getDefinition(term: string) {
+		const lowercaseTerm = term.toLowerCase();
+		const singularTerm =
+			lowercaseTerm.charAt(lowercaseTerm.length - 1) !== "s"
+				? lowercaseTerm
+				: lowercaseTerm.slice(0, -1);
+		const pluralTerm = singularTerm + "s";
+
+		if (singularTerm in def_dict) return def_dict[singularTerm];
+		return def_dict[pluralTerm];
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.scanDefinitions();
+		this.addRibbonIcon("list-restart", "Rescan Definitions", () => {
+			this.scanDefinitions();
+			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+			view?.previewMode.rerender(true);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerMarkdownPostProcessor((el, ctx) => {
+			const codeblocks = el.findAll("code");
+			for (let codeblock of codeblocks) {
+				const text = codeblock.innerText.trim();
+				if (text[0] === "%" && text[text.length - 1] === "%") {
+					const term = text.substring(1, text.length - 1).trim();
+					const newText = codeblock.createSpan({
+						text: term,
+						cls: "definition",
+					});
+					newText.style.color = this.settings.def_color;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+					newText.addEventListener("mouseenter", () => {
+						// Create and show a tooltip on mouseenter
+						const tooltip = document.createElement("div");
+						const def = this.getDefinition(term);
+						tooltip.textContent = def
+							? def
+							: "No definition found :/";
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+						tooltip.classList.add("tooltip");
+
+						document.body.appendChild(tooltip);
+
+						// Position the tooltip relative to the hovered element
+						const rect = newText.getBoundingClientRect();
+						tooltip.style.left = rect.left + "px";
+						tooltip.style.top = rect.bottom + window.scrollY + "px";
+					});
+
+					newText.addEventListener("mouseleave", () => {
+						const tooltip = document.querySelector(".tooltip");
+						if (tooltip) {
+							tooltip.remove();
+						}
+					});
+					codeblock.replaceWith(newText);
 				}
 			}
 		});
 
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				if (file instanceof TFile) {
+					menu.addItem((item) => {
+						item.setIcon("list-restart");
+						item.setTitle("Scan for unmarked definitions");
+						item.onClick(async () => {
+							await this.app.vault
+								.read(file)
+								.then((content: string) => {
+									let contentCopy = content;
+									for (const [term, _] of Object.entries(
+										def_dict
+									)) {
+										contentCopy = contentCopy.replace(
+											new RegExp(term, "gi"),
+											"`%" + term + "%`"
+										);
+									}
+									contentCopy = contentCopy
+										.replace(
+											new RegExp("`%" + "`%", "gi"),
+											"`%"
+										)
+										.replace(
+											new RegExp("\\[\\[" + "`%", "gi"),
+											"[["
+										)
+										.replace(
+											new RegExp("%`" + "%`", "gi"),
+											"%`"
+										)
+										.replace(
+											new RegExp("%`" + "\\]\\]", "gi"),
+											"]]"
+										);
+
+									const newPath = file.path.replace(
+										".md",
+										"_copy.md"
+									);
+									this.app.vault.create(newPath, contentCopy);
+								});
+						});
+					});
+				}
+			})
+		);
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +188,43 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: DefinitionPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DefinitionPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Definitions File Name")
+			.setDesc(
+				"Markdown files with this name will be scanned for definitions."
+			)
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.filename)
+					.onChange(async (value) => {
+						this.plugin.settings.filename = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Definition Color")
+			.setDesc("")
+			.addColorPicker((cb) => {
+				cb.setValue(this.plugin.settings.def_color).onChange(
+					async (value) => {
+						this.plugin.settings.def_color = value;
+						await this.plugin.saveSettings();
+					}
+				);
+			});
 	}
 }
